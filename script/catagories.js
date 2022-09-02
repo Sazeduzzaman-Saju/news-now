16368a5e6ebbee4e6e70caeedb4568f9bbc7b1c5
sazeduzzaman <sazeduzzaman@yahoo.com>
2022-09-03 02:08:55 +0600

    News Part Done

--- script/catagories.js
+++ script/catagories.js
@@ -7,7 +7,6 @@ const loadCatagories = ()=>{
 }
 
 const displayNavigation =(nav)=>{
-    console.log(nav);
     const getNavContainer = document.getElementById('nav-container');
     getNavContainer.innerHTML= `
             <nav class="navbar navbar-expand-lg" id="myHeader">
@@ -17,35 +16,34 @@ const displayNavigation =(nav)=>{
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mx-auto mb-2 mb-lg-0 ">
-                    <li class="nav-item me-2">
-                        <a class="nav-link active" id="active" aria-current="page" href="#">${nav[0].category_name}</a>
+                    <li class="nav-item navlinks">
+                        <a class="nav-link active navlinks-start" id="active" aria-current="page" href="#">Home</a>
                     </li>
-                    <li class="nav-item me-2">
+                    <li class="nav-item navlinks" onclick="loadNews('${nav[0].category_id}')">
                         <a class="nav-link " id="" aria-current="page" href="#">${nav[0].category_name}</a>
                     </li>
-                    <li class="nav-item me-2">
+                    <li class="nav-item navlinks" onclick="loadNews('${nav[1].category_id}')">
                         <a class="nav-link " id="" aria-current="page" href="#">${nav[1].category_name}</a>
                     </li>
-                    <li class="nav-item me-2">
+                    <li class="nav-item navlinks" onclick="loadNews('${nav[2].category_id}')">
                         <a class="nav-link " id="" aria-current="page" href="#">${nav[2].category_name}</a>
                     </li>
-                    <li class="nav-item me-2">
+                    <li class="nav-item navlinks" onclick="loadNews('${nav[3].category_id}')">
                         <a class="nav-link " id="" aria-current="page" href="#">${nav[3].category_name}</a>
                     </li>
-                    <li class="nav-item me-2">
+                    <li class="nav-item navlinks" onclick="loadNews('${nav[4].category_id}')">
                         <a class="nav-link " id="" aria-current="page" href="#">${nav[4].category_name}</a>
                     </li>
-                    <li class="nav-item me-2">
+                    <li class="nav-item navlinks" onclick="loadNews('${nav[5].category_id}')">
                         <a class="nav-link " id="" aria-current="page" href="#">${nav[5].category_name}</a>
                     </li>
-                    <li class="nav-item me-2">
+                    <li class="nav-item navlinks" onclick="loadNews('${nav[6].category_id}')">
                         <a class="nav-link " id="" aria-current="page" href="#">${nav[6].category_name}</a>
                     </li>
-                    <li class="nav-item me-2">
-                        <a class="nav-link " id="" aria-current="page" href="#">${nav[7].category_name}</a>
+                    <li class="nav-item navlinks" onclick="loadNews('${nav[7].category_id}')">
+                        <a class="nav-link navlinks-end" id="" aria-current="page" href="#">${nav[7].category_name}</a>
                     </li>
                 </ul>
-
             </div>
             </div>
         </nav>
@@ -53,5 +51,75 @@ const displayNavigation =(nav)=>{
     getNavContainer.appendChild(getNavContainer);
 
 }
+
+
+// 
+const loadNews = (catagoriId) =>{
+    const url =`https://openapi.programming-hero.com/api/news/category/${catagoriId}`;
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=> newsContainer(data.data));
+}
+
+const newsContainer=(allNews)=>{
+    const getNewsContainer = document.getElementById('news-container');
+    getNewsContainer.innerHTML='';
+    allNews.forEach(news => {
+        const createElement = document.createElement('div');
+        createElement.innerHTML=`
+        <div class="col-lg-12 col-md-12 col-sm-12">
+        <div class="card mb-3 mt-5 shadow-lg border-0">
+            <div class="row g-0">
+              <div class="col-md-3">
+                <img src="${news.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+              </div>
+              <div class="col-md-9">
+                <div class="card-body">
+                  <h5 class="card-title fw-bold">${news.title}</h5>
+                  <p class="card-text">${news.details.slice(0,480)}</p>
+                  <div class="row">
+                    <div class="col-lg-3 col-md-3 col-sm-6 d-flex justify-content-center align-items-center">
+                        <img class="w-25 rounded-circle" src="${news.author.img}" alt="">
+                        <div class="d-flex flex-column justify-content-start">
+                            <small>${news.author.name ? news.author.name : "stranger"}</small>
+                            <small>${news.author.published_date.slice(0,10)}</small>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-6 d-flex justify-content-center align-items-center">
+                        <div>
+                            <i class="fa-regular fa-eye"></i>
+                        </div>
+                        <div class="ms-2 mt-3">
+                            <p>${news.total_view}</p>
+                        </div>
+                        
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-6 d-flex justify-content-center align-items-center">
+                        <div>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-regular fa-star-half-stroke"></i>
+                        </div>
+                        <div class="ms-2">
+                            <i class="fa-regular fa-star-half-stroke"></i>
+                            <i class="fa-regular fa-star-half-stroke"></i>
+                            <i class="fa-regular fa-star-half-stroke"></i>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-6 d-flex justify-content-end align-items-center">
+                        <a href=""><i class="fa-solid fa-arrow-right pe-3"></i></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        `;
+        getNewsContainer.appendChild(createElement);
+        
+    });
+}
+
 
 loadCatagories();
